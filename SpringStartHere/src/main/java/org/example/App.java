ad4bbb9c3c2f262e76510373886932e93b3a476f
Ygor Nascimento <yna@Ygors-MacBook-Air.local>
2025-04-10 21:42:24 -0300

    Creating an instance of AnnotationConfigApplicationContext

--- SpringStartHere/src/main/java/org/example/App.java
+++ SpringStartHere/src/main/java/org/example/App.java
@@ -1,13 +1,14 @@
 package org.example;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
+        var context  = new AnnotationConfigApplicationContext();
         System.out.println( "Hello World!" );
     }
 }
