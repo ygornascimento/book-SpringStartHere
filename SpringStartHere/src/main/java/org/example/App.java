38fdfcbf0074dbbf4e994dcfe76d473a1e8adcaa
Ygor Nascimento <ygor.nascimento@outlook.com>
2025-04-13 13:24:29 -0300

    Adding instances of the same type to Context

--- SpringStartHere/src/main/java/org/example/App.java
+++ SpringStartHere/src/main/java/org/example/App.java
@@ -11,8 +11,11 @@ public class App {
     public static void main( String[] args ) {
         var context  = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        Parrot parrot = context.getBean(Parrot.class);
+        //Parrot parrot = context.getBean(Parrot.class);
+        Parrot parrot = context.getBean("parrot", Parrot.class);
+        Parrot parrot2 = context.getBean("parrot2", Parrot.class);
         System.out.println( parrot.getName() );
+        System.out.println( parrot2.getName() );
 
         String s = context.getBean(String.class);
         System.out.println(s);
--- SpringStartHere/src/main/java/org/example/config/ProjectConfig.java
+++ SpringStartHere/src/main/java/org/example/config/ProjectConfig.java
@@ -22,4 +22,18 @@ public class ProjectConfig {
     Integer ten() {
         return 10;
     }
+
+    @Bean
+    Parrot parrot2() {
+        var p = new Parrot();
+        p.setName( "Miki" );
+        return p;
+    }
+
+    @Bean
+    Parrot parrot3() {
+        var p = new Parrot();
+        p.setName( "Riki" );
+        return p;
+    }
 }
