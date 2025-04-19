872b0f52e184a77b4cdfe38cb3e3384cc57b6e66
Ygor Nascimento <ygor.nascimento@outlook.com>
2025-04-19 12:08:37 -0300

    Adding @PostConstruct annotation - if we want to execute some instructions right after Spring creates the bean we use @PostConstruct

--- SpringStartHere/src/main/java/org/example/Parrot.java
+++ SpringStartHere/src/main/java/org/example/Parrot.java
@@ -1,12 +1,20 @@
 package org.example;
 
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
 /*Using the @Component stereotype annotation over the class,
 we instruct Spring to create an instance of this class
 and add it to its Context*/
 @Component
 public class Parrot {
     private String name;
+
+    @PostConstruct
+    public void init() {
+        this.name = "Kiki";
+    }
 
     public String getName() {
         return name;
