33017e22bab4d165a93891418b0a1dc09bce1c4e
Ygor Nascimento <ygor.nascimento@outlook.com>
2025-05-07 09:06:39 -0300

    Chapter 8.1 - Implementing web apps with a dynamic view.

--- SpringStartHerePart2/src/main/java/com/example/springstartherepart2/controller/MainController.java
+++ SpringStartHerePart2/src/main/java/com/example/springstartherepart2/controller/MainController.java
@@ -1,13 +1,19 @@
 package com.example.springstartherepart2.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller// We annotate the class with the @Controller stereotype annotation.
+@Controller/*
+    The @Controller stereotype annotation marks this class as Spring MVC controller and adds a bean of this type
+    to the Spring Context.
+*/
 public class MainController {
 
-    @RequestMapping("/home") //We use the @RequestMapping annotation to associate the action with an HTTP request path.
-    public String home() {
-        return "home.html"; // We return the HTML document name that contains the details we want the browser to display.
+    @RequestMapping("/home") // We assign the controlle's action to an HTTP request path.
+    public String home(Model page) {
+        page.addAttribute("username", "Katy"); // Adding the data we want the controller to send to the view.
+        page.addAttribute("color", "red"); // Adding the data we want the controller to send to the view.
+        return "home.html"; // The controller's action returns the view to be rendered into the HTTP response.
     }
 }
