b3b7509f3ea435ce8de380b801beab4b53df18b6
Ygor Nascimento <ygor.nascimento@outlook.com>
2025-05-08 15:46:03 -0300

    Chapter 8.1.2 - Using request parameters to send data from client to server.

--- SpringStartHerePart2/src/main/java/com/example/springstartherepart2/controller/MainController.java
+++ SpringStartHerePart2/src/main/java/com/example/springstartherepart2/controller/MainController.java
@@ -3,6 +3,7 @@ package com.example.springstartherepart2.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller/*
     The @Controller stereotype annotation marks this class as Spring MVC controller and adds a bean of this type
@@ -11,9 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
 
     @RequestMapping("/home") // We assign the controlle's action to an HTTP request path.
-    public String home(Model page) {
-        page.addAttribute("username", "Katy"); // Adding the data we want the controller to send to the view.
-        page.addAttribute("color", "red"); // Adding the data we want the controller to send to the view.
+    public String home(@RequestParam String color, @RequestParam String name, Model page) {
+        page.addAttribute("username", name); // Adding the data we want the controller to send to the view.
+        page.addAttribute("color", color); // The controller passes the color sent by the cliente to the view.
         return "home.html"; // The controller's action returns the view to be rendered into the HTTP response.
     }
 }
