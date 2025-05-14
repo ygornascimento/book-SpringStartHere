cb99f08377e0ab31046f5fb21866bd5b3ef4aa2b
Ygor Nascimento <ygor.nascimento@outlook.com>
2025-05-14 09:01:53 -0300

    Chapter 10.3.3.1 - Separating the execption logic with a REST Controller Adivice Spring Aspect.

--- SpringStartHerePart2/src/main/java/com/example/springstartherepart2/controller/PaymentController.java
+++ SpringStartHerePart2/src/main/java/com/example/springstartherepart2/controller/PaymentController.java
@@ -21,17 +21,9 @@ public class PaymentController {
     /* In some cases, we need to configure a custom response status. The easiest and most
     common way to customize the HTTP response is using the ResponseEntity class. This class provided by Spring allows you to specify the
     response body, status, and the headers on the HTTP response.*/
-    public ResponseEntity<?> makePayment() {
-        try {
-            PaymentDetails paymentDetails = paymentService.processPayment(); // We try calling the processPayment() method of the service.
-            return ResponseEntity.status(HttpStatus.ACCEPTED) /* If calling the service method succeeds, we return an HTTP response
-            with status Accepted and the PaymentDetails instance as a response body. */
-                    .body(paymentDetails);
-        } catch (NotEnoughMoneyExecption e) {
-            ErrorDetails errorDetails = new ErrorDetails();
-            errorDetails.setMessage("Not enough money to make the payment.");
-            return ResponseEntity.badRequest().body(errorDetails); /*If an exception of type NotEnoughMoneyException is thrown, we
-            return an HTTP response with status Bad Request and an ErrorDetails instance. */
-        }
+    public ResponseEntity<PaymentDetails> makePayment() {
+        PaymentDetails paymentDetails = paymentService.processPayment();
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(paymentDetails);
     }
 }
---
+++ SpringStartHerePart2/src/main/java/com/example/springstartherepart2/execptions/ExceptionControllerAdvice.java
@@ -0,0 +1,17 @@
+package com.example.springstartherepart2.execptions;
+
+import com.example.springstartherepart2.model.ErrorDetails;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice // We use the @RestControllerAdvice annotation to mark the class as a REST controller advice.
+public class ExceptionControllerAdvice {
+    /* We use the @ExceptionHandler method to associate an exception with the logic the method implements.*/
+    @ExceptionHandler(NotEnoughMoneyExecption.class)
+    public ResponseEntity<ErrorDetails> exceptionNotEnoughMoney() {
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setMessage("Not enough money to make the payment.");
+        return ResponseEntity.badRequest().body(errorDetails);
+    }
+}
